87574296a48f9e90468efc3bc0b66a861a69ee60
PedroChacon <peemchaar2@gmail.com>
2024-03-20 19:22:38 -0400

    add scroll on drag feature, footer, home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IProject, ProjectsList } from 'src/app/interfaces/project.interface';
 
 @Component({
@@ -8,9 +8,37 @@ import { IProject, ProjectsList } from 'src/app/interfaces/project.interface';
 })
 export class HomeComponent implements OnInit {
   projectsList: Array<IProject> = new ProjectsList;
+
+  mouseDown = false;
+
+  startX: any;
+
+  scrollLeft: any;
+
+  @ViewChild('carrousel') slider: ElementRef | undefined;
+
+  startDragging(e:any, flag:any) {
+    this.mouseDown = true;
+    this.startX = e.pageX - this.slider?.nativeElement.offsetLeft;
+    this.scrollLeft = this.slider?.nativeElement.scrollLeft;
+  }
+  stopDragging(e:any, flag:any) {
+    this.mouseDown = false;
+  }
+  moveEvent(e:any) {
+    e.preventDefault();
+    if (!this.mouseDown) {
+      return;
+    }
+    const x = e.pageX - this.slider?.nativeElement.offsetLeft;
+    const scroll = x - this.startX;
+    this.slider!.nativeElement.scrollLeft = this.scrollLeft - scroll;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
