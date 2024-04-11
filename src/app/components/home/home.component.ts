9ec467a1328dae7e5bc50d9810f7d42f7ce8ddcd
PedroChacon <peemchaar2@gmail.com>
2024-04-11 13:27:32 -0400

    add: responsive design for (home, footer, project-card)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { HeaderService } from 'src/app/services/header.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   projectsList: Array<IProject> = new ProjectsList;
